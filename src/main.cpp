82f426292123328fb9f40e080c1db5ebf89883bd
Lsong <song940@gmail.com>
2024-12-09 22:10:44 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,37 @@ void setup()
   display->begin();
   display->setBrightness8(50);
   display->clearScreen();
-  display->setCursor(0, 0);
-  display->setTextColor(display->color444(0, 0, 255));
-  display->println("Hello");
+  // draw text with a rotating colour
+  display->setTextSize(1);     // size 1 == 8 pixels high
+  display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
+  display->setCursor(0, 0);    // start at top left, with 8 pixel of spacing
+  display->setTextColor(display->color444(15, 15, 15));
+  display->println("LED MATRIX!");
+
+  // print each letter with a fixed rainbow color
+  display->setTextColor(display->color444(0, 8, 15));
+  display->print('6');
+  display->setTextColor(display->color444(15, 4, 0));
+  display->print('4');
+  display->setTextColor(display->color444(15, 15, 0));
+  display->print('x');
+  display->setTextColor(display->color444(8, 15, 0));
+  display->print('6');
+  display->setTextColor(display->color444(8, 0, 15));
+  display->print('4');
+
+  // Jump a half character
+  display->setCursor(34, 24);
+  display->setTextColor(display->color444(0, 15, 15));
+  display->print("*");
+  display->setTextColor(display->color444(15, 0, 0));
+  display->print('R');
+  display->setTextColor(display->color444(0, 15, 0));
+  display->print('G');
+  display->setTextColor(display->color444(0, 0, 15));
+  display->print("B");
+  display->setTextColor(display->color444(15, 0, 8));
+  display->println("*");
 }
 
 void loop()
